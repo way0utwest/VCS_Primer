578d25b78f5036e235d2a933aaf49e41750e9d0b
steve jones <way0utwest@hotmail.com>
2023-09-23 09:25:51 -0600

    denver demo 2023 1

--- SQL/Tables/OrderHeader.sql
+++ SQL/Tables/OrderHeader.sql
@@ -1,19 +1,4 @@
-/****** Object:  Table [dbo].[OrderHeader]    Script Date: 10/22/2021 3:15:35 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[OrderHeader](
-	[OrderID] [int] NOT NULL,
-	[OrderDate] [datetime] NULL,
-	[CustomerID] [int] NULL,
-	[OrderReceivedDate] [datetime] NULL,
-	[OrderUpdatedDate] [datetime] NULL,
-	[OrderProcessDate] [datetime] NULL,
-	[OrderCompleteDate] [datetime] NULL
-	, Status tinyint
-) ON [PRIMARY]
+-- 2023-09-23 added new colum for demo
+ALTER TABLE dbo.OrderHeader ADD denveraddress VARCHAR(1000)
 GO
 
